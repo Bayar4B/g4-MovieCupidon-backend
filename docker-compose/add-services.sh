1a24793529107622acec1554384d9f3b0b212f87
Rolando Grave De Peralta Gonzalez <rolando.grave@etu.unige.ch>
2021-04-10 19:42:46 +0000

    Update build_run.sh, add-services.sh, and kill_all.sh
    Vous pouvez run avec build_run.sh.
    Vous pouvez clean toutes les images et docker containers en runnant 2 fois kill_all.sh
    Pensez à run add-services.sh au cas ou il a été run trop tôt.
    Consultez le readme pour plus d'infos.
    + Support pour WSL normalement

--- docker-compose/add-services.sh
+++ docker-compose/add-services.sh
@@ -1,11 +1,23 @@
 curl -XPOST "http://localhost:8001/services" --header "Content-Type: application/json" --data '
 {
-  "name": "testy2",       
+  "name": "lobby-service",       
   "url": "http://host.docker.internal:8080/"
 }'
 
-curl -XPOST "http://localhost:8001/services/testy2/routes" --header "Content-Type: application/json" --data '
+curl -XPOST "http://localhost:8001/services/lobby-service/routes" --header "Content-Type: application/json" --data '
 {
-  "paths": ["/ap/testy"]
+  "paths": ["/lobby"]
+}
+'
+
+curl -XPOST "http://localhost:8001/services" --header "Content-Type: application/json" --data '
+{
+  "name": "sample-selection-service",       
+  "url": "http://host.docker.internal:8080/"
+}'
+
+curl -XPOST "http://localhost:8001/services/sample-selection-service/routes" --header "Content-Type: application/json" --data '
+{
+  "paths": ["/lobby/sample-selection"]
 }
 '
